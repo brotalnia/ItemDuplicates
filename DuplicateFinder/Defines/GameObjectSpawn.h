f4067295d8e4d658290b92422cb0e8895b251b74
brotalnia <brotalnia@gmail.com>
2020-03-01 02:23:18 +0200

    More tabs to spaces.

--- DuplicateFinder/Defines/GameObjectSpawn.h
+++ DuplicateFinder/Defines/GameObjectSpawn.h
@@ -5,20 +5,20 @@ struct GameObjectSpawn
     uint32    guid = 0;
     uint32    id = 0;
     uint32    map = 0;
-	float     position_x = 0.0f;
-	float     position_y = 0.0f;
-	float     position_z = 0.0f;
-	float     orientation = 0.0f;
-	float     rotation0 = 0.0f;
-	float     rotation1 = 0.0f;
-	float     rotation2 = 0.0f;
+    float     position_x = 0.0f;
+    float     position_y = 0.0f;
+    float     position_z = 0.0f;
+    float     orientation = 0.0f;
+    float     rotation0 = 0.0f;
+    float     rotation1 = 0.0f;
+    float     rotation2 = 0.0f;
     float     rotation3 = 0.0f;
 
     bool operator==(const GameObjectSpawn& val) const
     {
         return (id == val.id) &&
                (map == val.map) &&
-			   (position_x == val.position_x) &&
+               (position_x == val.position_x) &&
                (position_y == val.position_y) &&
                (position_z == val.position_z) &&
                (orientation == val.orientation) &&
